7389d292075f336458b8a4de5762901229b5f3e6
이해민 <2haemin98@gmail.com>
2022-10-09 15:48:24 +0900

    online LIS, Floyd algorithm

---
+++ algorithm_1006/src/algorithm/LIS/LISTest2.java
@@ -0,0 +1,33 @@
+package algorithm.LIS;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LISTest2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[] arr = new int[N];// 수열의 수들
+		int[] dp = new int[N];// 동적테이블 : 해당 길이를 만족하는 자리에 오는 수의 최소값
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int size = 0;
+		for (int i = 0; i < N; i++) {
+			int pos = Arrays.binarySearch(dp, 0, size, arr[i]);
+			if (pos >= 0)
+				continue;
+
+			int insertPos = Math.abs(pos) - 1;// 멘뒤 또는 기존 원소의 대체 자리
+			dp[insertPos] = arr[i];
+
+			if (insertPos == size)
+				size++;
+
+		}
+		System.out.println(size);
+	}
+}
